19c90e372cfb4476a7cfb63fad5d9723a4a837f8
onboss-digital <onbossdigital@gmail.com>
2026-07-20 02:44:40 -0300

    fix: porta dinamica para o cloudpanel

--- app.py
+++ app.py
@@ -259,4 +259,5 @@ def del_config():
 
 if __name__ == '__main__':
     # Para desenvolvimento
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 8090))
+    app.run(host='0.0.0.0', port=port, debug=True)
